fb5dd01fdd2fb3380c9633cd23c1559e855ce895
Samir Saad <samir.saadallah@gmail.com>
2020-06-04 23:32:20 -0400

    Fix sonar issues

--- src/main/java/org/temp/demo/myapp/api/HelloWorldController.java
+++ src/main/java/org/temp/demo/myapp/api/HelloWorldController.java
@@ -15,13 +15,14 @@ public class HelloWorldController {
 
     private final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
-    private static final String template = "Hello, %s!";
+    private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping
     @ResponseStatus(code = HttpStatus.OK)
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        name = name.replace( '\n' ,  '_' ).replace( '\r' , '_' ).replace( '\t' , '_' );
         logger.info("Greeting {}", name);
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
 }
